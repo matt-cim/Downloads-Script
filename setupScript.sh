dd6d6ebf10b73a3255256d762f4bd6fb1249c813
matt-cim <100395759+matt-cim@users.noreply.github.com>
2022-06-07 16:34:09 +0000

    Initial commit

---
+++ setupScript.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Setup script for setting up a new macos machine
+echo "Starting setup..."
+
+# this takes the most time to install
+xcode-select --install
+
+echo -n "Enter name of User ex: bob.smith "
+read answer
+
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"'     >> 
+    /Users/$answer/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Check if homebrew is installed, else install if it's missing
+if test ! $(which brew); then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL     https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "updating brew..."
+brew update
+
+PACKAGES=(
+    git
+    tmux
+    bat
+    macvim
+    mysql
+    fzf
+    ctags
+    readline
+)
+echo "Installing packages..."
+brew install ${PACKAGES[@]}
+
+brew link --force readline
+
+CASKS=(
+    spotify
+    steam
+)
+
+echo "Installing necessary apps..."
+brew install --cask ${CASKS[@]}
